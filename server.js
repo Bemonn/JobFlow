edb9688bfdcb63a4639d8a6bd350f9c818836f7b
Bemon <tristan2107b@gmail.com>
2023-08-22 21:11:18 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+const exphbs = require('express-handlebars');
+const { Sequelize } = require('sequelize');
+const config = require('./config/config.json').development;
+
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: 'mysql',
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Basic server
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
