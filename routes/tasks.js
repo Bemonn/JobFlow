4814382c6e86a73d4634be6d7c2c719c293efeb8
njheymann <nils.heymann@Hotmail.com>
2023-08-28 22:01:53 +1000

    Frontend stuff

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-//importing from controller CRUD 
-const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/tasks');
+//importing from controller CRUD
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks");
 
-router.route('/').get(getAllTasks).post(createTask);
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
