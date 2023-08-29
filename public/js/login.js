a016bbaedd4becf7011edfce1d63d8b70da51693
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2023-08-29 20:59:00 +1000

    merged with errors

--- public/js/login.js
+++ public/js/login.js
@@ -1,56 +1,56 @@
 // login handler
 
 document
-  .getElementById("login-form")
-  .addEventListener("submit", async (event) => {
-    event.preventDefault();
+	.getElementById('login-form')
+	.addEventListener('submit', async (event) => {
+		event.preventDefault();
 
-    const form = document.getElementById("login-form");
-    const email = form.elements["email"].value;
-    const password = form.elements["password"].value;
+		const form = document.getElementById('login-form');
+		const email = form.elements['email'].value;
+		const password = form.elements['password'].value;
 
-    try {
-      const response = await fetch("/api/user/login", {
-        method: "POST",
-        body: JSON.stringify({ email, password }),
-        headers: { "Content-Type": "application/json" },
-      });
+		try {
+			const response = await fetch('/api/user/login', {
+				method: 'POST',
+				body: JSON.stringify({ email, password }),
+				headers: { 'Content-Type': 'application/json' },
+			});
 
-      if (!response.ok) {
-        throw new Error("There was an error logging in.");
-      }
+			if (!response.ok) {
+				throw new Error('There was an error logging in.');
+			}
 
-      document.location.replace("/employees");
-    } catch (err) {
-      console.log(err);
-    }
-  });
+			document.location.replace('/employees');
+		} catch (err) {
+			console.log(err);
+		}
+	});
 // signup handler
 
 // Event listener for signup button
 document
-  .getElementById("signup-form")
-  .addEventListener("submit", async (event) => {
-    event.preventDefault();
+	.getElementById('signup-form')
+	.addEventListener('submit', async (event) => {
+		event.preventDefault();
 
-    const form = document.getElementById("signup-form");
-    const username = form.elements["username"].value;
-    const password = form.elements["password"].value;
-    const confirmPassword = form.elements["confirm_password"].value;
+		const form = document.getElementById('signup-form');
+		const username = form.elements['username'].value;
+		const password = form.elements['password'].value;
+		const confirmPassword = form.elements['confirm_password'].value;
 
-    try {
-      const response = await fetch("/api/user/signup", {
-        method: "POST",
-        body: JSON.stringify({ username, password, confirmPassword }),
-        headers: { "Content-Type": "application/json" },
-      });
+		try {
+			const response = await fetch('/api/user/signup', {
+				method: 'POST',
+				body: JSON.stringify({ username, password, confirmPassword }),
+				headers: { 'Content-Type': 'application/json' },
+			});
 
-      if (!response.ok) {
-        throw new Error("There was an error signing up.");
-      }
+			if (!response.ok) {
+				throw new Error('There was an error signing up.');
+			}
 
-      document.location.replace("/employees");
-    } catch (err) {
-      console.log(err);
-    }
-  });
+			document.location.replace('/employees');
+		} catch (err) {
+			console.log(err);
+		}
+	});
