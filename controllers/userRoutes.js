7614628635b9e8be655c27ac00ccd6e0c1acb180
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2023-08-29 22:52:26 +1000

    fixed integration issues, handlebar now works on the local host

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,77 +1,77 @@
 // This handles the login information for the user
 
-const router = require('express').Router();
-const { Employee } = require('../../models');
+const router = require("express").Router();
+const { Employee } = require("../../models");
 
 // POST /api/user/login
 
-router.post('/login', async (req, res) => {
-	try {
-		const { email, password } = req.body;
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-		const employeeData = await Employee.findOne({ where: { email } });
+    const employeeData = await Employee.findOne({ where: { email } });
 
-		if (!employeeData || !employeeData.checkPassword(password)) {
-			res.status(400).json({ message: 'Incorrect email or password!' });
-			return;
-		}
+    if (!employeeData || !employeeData.checkPassword(password)) {
+      res.status(400).json({ message: "Incorrect email or password!" });
+      return;
+    }
 
-		req.session.save(() => {
-			req.session.user_id = employeeData.id;
-			req.session.logged_in = true;
-			res.json({ employee: employeeData, message: 'You are now logged in!' });
-		});
-	} catch (err) {
-		res.status(400).json(err);
-	}
+    req.session.save(() => {
+      req.session.user_id = employeeData.id;
+      req.session.logged_in = true;
+      res.json({ employee: employeeData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // POST /api/user/logout
 
-router.post('/logout', (req, res) => {
-	if (req.session.logged_in) {
-		req.session.destroy(() => {
-			res.status(204).end();
-		});
-	} else {
-		res.status(404).end();
-	}
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 // POST /api/user/signup
 
-router.post('/signup', async (req, res) => {
-	try {
-		const {
-			first_name,
-			last_name,
-			email,
-			password,
-			confirm_password,
-			position,
-		} = req.body;
+router.post("/signup", async (req, res) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      confirm_password,
+      position,
+    } = req.body;
 
-		if (password !== confirm_password) {
-			res.status(400).json({ message: 'Passwords do not match!' });
-			return;
-		}
+    if (password !== confirm_password) {
+      res.status(400).json({ message: "Passwords do not match!" });
+      return;
+    }
 
-		const employeeData = await Employee.create({
-			first_name,
-			last_name,
-			email,
-			password,
-			position,
-		});
+    const employeeData = await Employee.create({
+      first_name,
+      last_name,
+      email,
+      password,
+      position,
+    });
 
-		req.session.save(() => {
-			req.session.user_id = employeeData.id;
-			req.session.logged_in = true;
-			res.json(employeeData);
-		});
-	} catch (err) {
-		res.status(400).json(err);
-	}
+    req.session.save(() => {
+      req.session.user_id = employeeData.id;
+      req.session.logged_in = true;
+      res.json(employeeData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 
 const session = require("express-session"); // import express-session
 const exphbs = require("express-handlebars");
 const tasks = require("./controllers/tasksRoutes");
 const sequelize = require("./config/connection");
+const routes = require("./controllers");
 
 const app = express();
 
@@ -17,31 +19,34 @@ app.engine("handlebars", hbs);
 app.set("view engine", "handlebars");
 
 // Use express-session middleware
-app.use(
-  session({
-    secret: "your_secret_key", // this secret will be used for signing the session ID cookie. Change it to your own secret string.
-    resave: false, // force the session to be saved back to the session store
-    saveUninitialized: false, // save uninitialized session to the store
-    cookie: { secure: "auto" }, // automatically set the cookie as secure if the request is secure
-  }),
-);
+// app.use(
+//   session({
+//     secret: "your_secret_key", // this secret will be used for signing the session ID cookie. Change it to your own secret string.
+//     resave: false, // force the session to be saved back to the session store
+//     saveUninitialized: false, // save uninitialized session to the store
+//     cookie: { secure: "auto" }, // automatically set the cookie as secure if the request is secure
+//   }),
+// );
 
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
 // Basic server routes
-app.get("/", (req, res) => {
-  // Counting views for the current session
-  req.session.views = (req.session.views || 0) + 1;
-  res.send(
-    `Hello World! You have viewed this page ${req.session.views} times.`,
-  );
-});
+// app.get("/", (req, res) => {
+//   // Counting views for the current session
+//   req.session.views = (req.session.views || 0) + 1;
+//   res.send(
+//     `Hello World! You have viewed this page ${req.session.views} times.`,
+//   );
+// });
 
-app.use("/tasks", tasks);
+// app.use("/tasks", tasks);
 
 sequelize
-  .sync({ force: false })
+  .sync({ force: true })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
