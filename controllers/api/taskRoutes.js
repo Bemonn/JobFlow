2e8ba1b24c254caf7ade262e582300f954db2481
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-08-30 22:02:02 +1000

    added homeroutes, spliced code with pre-existing code, cleaned code for routes/api, still needs more work

--- controllers/api/taskRoutes.js
+++ controllers/api/taskRoutes.js
@@ -3,38 +3,38 @@ const express = require("express");
 const router = express.Router();
 const { Task } = require("../../models");
 
-// Get information for all tasks
-router.get("/tasks", async (req, res) => {
-  try {
-    const taskData = await Task.findAll();
-    if (!taskData) {
-      res.status(404).json({ message: "No tasks found!" });
-      return;
-    }
-    res.status(200).json(taskData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// // Get information for all tasks
+// router.get("/tasks", async (req, res) => {
+//   try {
+//     const taskData = await Task.findAll();
+//     if (!taskData) {
+//       res.status(404).json({ message: "No tasks found!" });
+//       return;
+//     }
+//     res.status(200).json(taskData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-// Get information for one tasks
-router.get("/tasks/:id", async (req, res) => {
-  try {
-    const taskData = await Task.findOne({
-      where: { id: req.params.id },
-    });
-    if (!taskData) {
-      res.status(404).json({ message: `No tasks found with this id: ${req.params.id}!` });
-      return;
-    }
-    res.status(200).json(taskData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// // Get information for one tasks
+// router.get("/tasks/:id", async (req, res) => {
+//   try {
+//     const taskData = await Task.findOne({
+//       where: { id: req.params.id },
+//     });
+//     if (!taskData) {
+//       res.status(404).json({ message: `No tasks found with this id: ${req.params.id}!` });
+//       return;
+//     }
+//     res.status(200).json(taskData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // Create a task
-router.post("/tasks", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const taskData = await Task.create({
       task_name: req.task_name,
@@ -48,8 +48,18 @@ router.post("/tasks", async (req, res) => {
   }
 });
 
+// // Create task
+// router.post("/", async (req, res) => {
+//   try {
+//     const task = await Task.create(req.body);
+//     res.status(201).json({ task });
+//   } catch (error) {
+//     res.status(500).json({ msg: error });
+//   }
+// });
+
 // Update a task
-router.put("/tasks/:id", async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const taskData = await Task.update(req.body, {
       where: { id: req.params.id },
@@ -60,6 +70,15 @@ router.put("/tasks/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// // Update task
+// router.patch("/:id", async (req, res) => {
+//   try {
+//     res.status(200).json({ msg: "Update logic needed" });
+//   } catch (error) {
+//     res.status(500).json({ msg: error });
+//   }
+// });
 
 // Delete a task
 router.delete("/tasks/:id", async (req, res) => {
@@ -76,5 +95,24 @@ router.delete("/tasks/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// // Delete task
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const deleteData = await Task.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
+//     if (!deleteData) {
+//       res.status(404).json({ message: "No task found with this id!" });
+//       return;
+//     }
+//     res.status(200).json(deleteData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
