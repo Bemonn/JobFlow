4e0af332d4aa62120288781687f171d333d565bb
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2023-08-30 22:54:56 +1000

    added more seed data

--- seeders/seed-employee-tasks.js
+++ seeders/seed-employee-tasks.js
@@ -1,6 +1,22 @@
 const employeeTaskData = [
-  { employee_id: 1, task_id: 1 },
+  { employee_id: 1, task_id: 2 },
+  { employee_id: 1, task_id: 4 },
   { employee_id: 2, task_id: 2 },
+  { employee_id: 2, task_id: 5 },
+  { employee_id: 3, task_id: 1 },
+  { employee_id: 3, task_id: 2 },
+  { employee_id: 3, task_id: 3 },
+  { employee_id: 3, task_id: 4 },
+  { employee_id: 4, task_id: 3 },
+  { employee_id: 4, task_id: 6 },
+  { employee_id: 5, task_id: 7 },
+  { employee_id: 6, task_id: 8 },
+  { employee_id: 6, task_id: 3 },
+  { employee_id: 7, task_id: 9 },
+  { employee_id: 7, task_id: 8 },
+  { employee_id: 8, task_id: 1 },
+  { employee_id: 8, task_id: 2 },
+  { employee_id: 8, task_id: 10 },
 ];
 
 module.exports = employeeTaskData;
